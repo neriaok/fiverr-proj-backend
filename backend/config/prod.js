b36d89405510ff44933674c9c1d452c08bfcba61
neriaok <neriaokabik.com>
2025-03-11 21:19:14 +0200

    Update atlas and data

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://team:1234@cluster0.t1t0n.mongodb.net/',
   dbName : process.env.DB_NAME || 'gig_DB'
 }
